b8c7d8b7488e3fb05b359caba1cab8b76304ccd2
aksisoftsby <aksisoftsby@gmail.com>
2021-04-03 08:25:18 +0700

    ok

---
+++ furni/clib.sh
@@ -0,0 +1,87 @@
+mkdir invitation
+cd invitation
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains fraqrancetheme.com --no-parent http://theone.fragrancetheme.com/wedding-invitation/
+cd ../
+
+mkdir taysta
+cd taysta
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpopal.com --no-parent https://demo2.wpopal.com/taysta/
+cd ../
+
+mkdir splits
+cd splits
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains fraqrancetheme.com --no-parent http://theone.fragrancetheme.com/split-screen/
+cd ../
+
+mkdir announcement
+cd announcement
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains fraqrancetheme.com --no-parent http://theone.fragrancetheme.com/announcement/
+cd ../
+
+mkdir announcement
+cd announcement
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains fraqrancetheme.com --no-parent http://theone.fragrancetheme.com/announcement/
+cd ../
+
+mkdir homeinvite
+cd homeinvite
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpbingosite.com --no-parent https://wpbingosite.com/wordpress/monisa/home-invitation/
+cd ../
+
+mkdir homeannounce
+cd homeannounce
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpbingosite.com --no-parent https://wpbingosite.com/wordpress/monisa/home-announcement/
+cd ../
+
+mkdir thehert
+cd thehert
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains pixiefy.com --no-parent https://pixiefy.com/themes/the-heart/onepage/index.html
+cd ../
+
+mkdir jevelin
+cd jevelin
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains shufflehound.com --no-parent https://jevelin.shufflehound.com/blog/blog-grid/
+cd ../
+
+mkdir exsythemes
+cd exsythemes
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains exsythemes.com --no-parent https://mywedding-exsy.exsythemes.com/v2_slider_bg/index.html
+cd ../
+
+mkdir unlockdesizn
+cd unlockdesizn
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains unlockdesizn.com --no-parent http://unlockdesizn.com/html/wedding/couple-heart/demo/ulockd-slider-split.html
+cd ../
+
+wget --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains kodeforest.net --no-parent http://kodeforest.net/html/eco-friendly/index.html
+
+
+############################
+
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpopal.com --no-parent https://demo2.wpopal.com/auros/
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpbingosite.com --no-parent https://wpbingosite.com/wordpress/davici/
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpbingosite.com --no-parent https://wpbingosite.com/wordpress/funio/
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains wpbingosite.com --no-parent https://wpbingosite.com/wordpress/funio/
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains qodeinteractive.com --no-parent https://konsept.qodeinteractive.com/furniture-store/
+
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains designesia.com --no-parent https://www.designesia.com/themes/archi-main/08_index-architect.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains elathemes.com --no-parent http://www.elathemes.com/themes/mobel/
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains htmlbeans.com --no-parent http://htmlbeans.com/html/schon/homepage1.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains hasthemes.com --no-parent https://htmldemo.hasthemes.com/lezada-preview/lezada/index-smart.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains kute-themes.com --no-parent http://kute-themes.com/html/luckyshop/html/index2.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains hasthemes.com --no-parent https://htmldemo.hasthemes.com/learts/learts/index-2.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains hasthemes.com --no-parent https://live.hasthemes.com/html/7/helendo-preview/helendo/index.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains hasthemes.com --no-parent https://live.hasthemes.com/html/13/zyra-preview/zyra/index-9.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains mysitesweb.su --no-parent https://demo-4.mysitesweb.su/index.html
+
+wget --reject-regex "(.*)\?(.*)" --recursive --page-requisites --adjust-extension --span-hosts --convert-links --restrict-file-names=windows --domains themebeyond.com --no-parent https://themebeyond.com/html/venam/special.html
